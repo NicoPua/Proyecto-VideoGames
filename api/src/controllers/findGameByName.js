2d3d578ef752961b4228e190af26678a0351009b
Nicolas Pua <nicopua7@gmail.com>
2023-04-12 19:16:34 -0300

    findGameByName function

---
+++ api/src/controllers/findGameByName.js
@@ -0,0 +1,18 @@
+const axios = require("axios");
+const {Videogame} = require("../db.js");
+const cleanDataGame = require("./cleanDataGame.js");
+
+const {API_KEY} = process.env;
+
+const findGameByName = async (name) =>{
+    let gameByNameAPI = (await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&search=${name}`)).data.results;
+    gameByNameAPI = await cleanDataGame(gameByNameAPI);       //Filtro los datos que necesito
+    let gameByNameBD = await Videogame.findAll({ where: { name } })
+
+    if(!gameByNameAPI.length && !gameByNameBD.length) throw new Error("This game doesn't exists")
+
+    const gameByName = [...gameByNameAPI, ...gameByNameBD];
+    return gameByName;
+}
+
+module.exports = findGameByName;
