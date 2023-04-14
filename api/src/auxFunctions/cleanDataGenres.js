d72b17c9496a1ae658ead2cf557acf60bd18fb33
Nicolas Pua <nicopua7@gmail.com>
2023-04-14 16:48:39 -0300

    cleanDataGenres

---
+++ api/src/auxFunctions/cleanDataGenres.js
@@ -0,0 +1,7 @@
+const cleanDataGenres = (arrDataGenres) => {
+    const arrGenres = [];
+    arrDataGenres.map((genre)=> arrGenres.push(genre.name));
+    return arrGenres;
+}
+
+module.exports = cleanDataGenres;
