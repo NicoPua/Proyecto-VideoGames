76adfa2c59e0cd4c7f63577430c7c86ffb5754f7
Nicolas Pua <nicopua7@gmail.com>
2023-04-15 21:55:55 -0300

    Update 15/04/2023

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
 import './App.css';
+import axios from "axios";
+
 import Landing from "./views/Landing/Landing.jsx";
 import About from "./views/About/About.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
-
+import Home from "./views/Home/Home.jsx"
 
 import { Switch, Route, useLocation} from "react-router-dom";
+import { useState } from 'react';
 
 function App() {
-  
+  const [games, setGames] = useState([]);
+
+  const onSearch = (name) => {
+    const URL = "http://localhost:3001";
+    if(games.find((game) => game.name == name)) return alert("This game already exists.")
+    
+    axios.get(`${URL}/videogames?name=${name}`)
+    .then((response) => {
+      let arrGames = response.data;
+      const existGame = arrGames.find((game) => game.name == name )
+      if(!existGame) return alert("This game doesn't exist.")
+
+      arrGames = arrGames.sort((g1, g2) => { 
+        if (g1.name < g2.name) {
+        return 1;
+        }
+        if (g1.name > g2.name) {
+          return -1;
+        }
+        return 0; }
+      )
+      setGames(arrGames);
+    })
+  }
+
   const location = useLocation();
   return (
       <Switch>
         {
-          location.pathname !== '/'?
+          location.pathname !== '/' ?
           <>
-          <Route path='/' component={NavBar}/> 
+          <Route path='/'>
+            <NavBar onSearch={onSearch}/>
+          </Route> 
           </> :
           <>
           <Route exact path="/" component={Landing}/>
           </>
         }
+        <Route path="/home" >
+          <Home games={games} />
+        </Route>
+
         <Route path="/about" component={About}/>
       </Switch>
   );
