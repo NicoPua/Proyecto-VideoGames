99a87daff522287b22e5aeba1547dfbfdd8b2845
Nicolas Pua <nicopua7@gmail.com>
2023-04-16 18:22:31 -0300

    Update 16/04/2023

--- api/src/controllers/findGameByName.js
+++ api/src/controllers/findGameByName.js
@@ -5,15 +5,17 @@ const cleanDataGame = require("./cleanDataGame.js");
 const {API_KEY} = process.env;
 
 const findGameByName = async (name) =>{
+    const cleanGameAPI = [];
     let gameByNameAPI = (await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&search=${name}`)).data.results;
     
-    gameByNameAPI = await cleanDataGame(gameByNameAPI);       //Filtro los datos que necesito
+    gameByNameAPI = await cleanDataGame(gameByNameAPI);                     //Limpio y filtro los datos que necesito.
+    for (let i = 0; i < 15; i++) { cleanGameAPI[i] = gameByNameAPI[i] }     //Filtro los primeros 15 juegos en un nuevo array.
 
-    let gameByNameBD = await Videogame.findAll({ where: { name } });
+    let gameByNameBD = await Videogame.findAll({ where: { name } });        //Busco el name del juego en la DB
 
-    if(!gameByNameAPI.length && !gameByNameBD.length) throw new Error("This game doesn't exists")
+    if(!cleanGameAPI.length && !gameByNameBD.length) throw new Error("This game doesn't exists")//Si no encuentra el juego, error.
 
-    const gameByName = [...gameByNameAPI, ...gameByNameBD];
+    const gameByName = [...cleanGameAPI, ...gameByNameBD];  //Junto elementos de ambos arrays para luego enviarlo.
     return gameByName;
 }
 
