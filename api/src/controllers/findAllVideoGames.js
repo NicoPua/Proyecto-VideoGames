69b9620de8d0174cd651e8b552bdfe9d28ee04da
Nicolas Pua <nicopua7@gmail.com>
2023-04-17 19:57:38 -0300

    findAllVideogames

--- api/src/controllers/findAllVideoGames.js
+++ api/src/controllers/findAllVideoGames.js
@@ -1,11 +1,18 @@
-const {Videogame} = require("../db.js");
+const {Videogame,Genre} = require("../db.js");
 
 const gamesPageFilter = require("../auxFunctions/gamesPagesFilter.js");
 
-
 const findAllVideoGames = async () => {
     let allGamesAPI = await gamesPageFilter();
-    const allGamesDB = await Videogame.findAll(); 
+    const allGamesDB = await Videogame.findAll({
+        include: {
+            model: Genre,
+            attributes: ["name"],
+            through: {
+                attributes: []
+            }
+        }
+    }); 
    
     const allVideogames = [...allGamesAPI, ...allGamesDB]
     return allVideogames;
