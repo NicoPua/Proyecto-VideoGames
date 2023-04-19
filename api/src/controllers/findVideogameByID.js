03b2c643f5663d1a643aded49a283b0ebcee7c7b
Nicolas Pua <nicopua7@gmail.com>
2023-04-19 14:54:10 -0300

    Update 19/04/2023

--- api/src/controllers/findVideogameByID.js
+++ api/src/controllers/findVideogameByID.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const {Videogame,Genre} = require("../db.js");
+const cleanDataGameAPI = require("../auxFunctions/cleanDataGameAPI.js");
 const { API_KEY } = process.env;
 
 const findVideogameByID = async (idValue,idType) =>{
@@ -17,8 +18,8 @@ const findVideogameByID = async (idValue,idType) =>{
 
     }else if(idType == 'API'){
         let arrGameAPI = (await axios.get(`https://api.rawg.io/api/games/${idValue}?key=${API_KEY}`)).data;
-        const {id,name,released,background_image,platforms,rating,genres,description} = arrGameAPI;
-        return {id,name,released,background_image,platforms,rating,genres,description,createinDb: false}; 
+        const cleanedData = cleanDataGameAPI(arrGameAPI);
+        return cleanedData; 
     }
 }
 
